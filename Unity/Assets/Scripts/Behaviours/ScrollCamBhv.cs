7d3f0f1c9d46e5a3011e38e87c8c394fbccadf54
Tim Steen <tls8963@rit.edu>
2015-03-16 22:52:31 -0400

    +Clamp Boundaries +Scroll zones +Prefab

--- Unity/Assets/Scripts/Behaviours/ScrollCamBhv.cs
+++ Unity/Assets/Scripts/Behaviours/ScrollCamBhv.cs
@@ -2,20 +2,48 @@
 using System.Collections;
 
 public class ScrollCamBhv : MonoBehaviour {
+	float m_constantScale = 100;
 	public float m_scrollSpeed;
 	public float m_scrollWidth;
 	public float m_scrollHeight;
+	public bool m_inclusive;
 	Camera m_camera;
-
+	
+	void clamp()
+	{
+		Vector3 camPos = m_camera.transform.position;
+		float halfWidth = m_scrollWidth / 2 / m_constantScale;
+		float halfHeight = m_scrollHeight / 2 / m_constantScale;
+		float padding = 0;
+		float aspectRatio = Camera.main.aspect;
+		//add padding to the boundaries
+		if (m_inclusive)
+			padding = Camera.main.orthographicSize;
+		float left = -halfWidth + padding * aspectRatio;
+		float right = halfWidth - padding * aspectRatio;
+		float top = halfHeight - padding;
+		float bottom = -halfHeight + padding;
+		if(camPos.x < left)
+			camPos.x = left;
+		if(camPos.x > right)
+			camPos.x = right;
+		if(camPos.y < bottom)
+			camPos.y = bottom;
+		if(camPos.y > top)
+			camPos.y = top;
+		m_camera.transform.position = camPos;
+	}
+	
+	void refreshCamera()
+	{
+		m_camera = Camera.main;
+		if(m_camera==null)
+			throw new UnassignedReferenceException("Scroll camera reference is null.");
+	}
+	
 	// Use this for initialization
 	void Start () {
 		refreshCamera();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		refreshCamera();
-		clamp();
 	}
 
 	public void Scroll(bool a_right, bool a_left, bool a_up, bool a_down)
@@ -29,32 +57,12 @@ public class ScrollCamBhv : MonoBehaviour {
 			movement.y += m_scrollSpeed;
 		if(a_down)
 			movement.y -= m_scrollSpeed;
-		m_camera.transform.Translate(movement);
-		Debug.Log("Cam Pos: " + m_camera.transform.position);
+		m_camera.transform.Translate(movement/m_constantScale);
 	}
-
-	void clamp()
-	{
-		Vector3 camPos = m_camera.transform.position;
-		float left = -m_scrollWidth;
-		float right = m_scrollWidth;
-		float top = m_scrollHeight;
-		float bottom = -m_scrollHeight;
-		if(camPos.x < left)
-			camPos.x = left;
-		if(camPos.x > right)
-			camPos.x = right;
-		if(camPos.y < bottom)
-			camPos.y = bottom;
-		if(camPos.y > top)
-			camPos.y = top;
-		m_camera.transform.position = camPos;
-	}
-
-	void refreshCamera()
-	{
-		m_camera = Camera.main;
-		if(m_camera==null)
-			throw new UnassignedReferenceException("Scroll camera reference is null.");
+	
+	// Update is called once per frame
+	void Update () {
+		refreshCamera();
+		clamp();
 	}
 }
--- Unity/Assets/Scripts/Controllers/ScrollCamCtrl.cs
+++ Unity/Assets/Scripts/Controllers/ScrollCamCtrl.cs
@@ -4,9 +4,23 @@ using System.Collections;
 public class ScrollCamCtrl : MonoBehaviour {
 	ScrollCamBhv m_scrollBhv;
 	Vector2 m_mousePos;
-	Vector2 m_mousePosOld;
-	Vector2 m_mouseChange;
-	public float padding;
+	public float m_padding = 25;
+	
+	void OnGUI(){
+		//create rectangles to indicate scroll regions
+		//Adapt these to the screens resolution
+		float innerBot = Screen.height - m_padding;
+		float innerRight = Screen.width - m_padding;
+		Rect topBar = new Rect (0, 0, Screen.width, m_padding);
+		Rect bottomBar = new Rect (0, innerBot, Screen.width, m_padding);
+		Rect leftBar = new Rect (0, m_padding, m_padding, innerBot - m_padding);
+		Rect rightBar = new Rect (innerRight, m_padding, m_padding, innerBot - m_padding);
+		//draw these rectangles as boxes
+		GUI.Box (topBar, string.Empty);
+		GUI.Box (bottomBar, string.Empty);
+		GUI.Box (leftBar, string.Empty);
+		GUI.Box (rightBar, string.Empty);
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -22,12 +36,8 @@ public class ScrollCamCtrl : MonoBehaviour {
 
 	void updateMouse()
 	{
-		//set old mouse to current
-		m_mousePosOld = m_mousePos;
 		//update current to present
 		m_mousePos = Input.mousePosition;
-		//get vector to current from old
-		m_mouseChange = m_mousePos - m_mousePosOld;
 	}
 
 	void moveByInput()
@@ -35,10 +45,10 @@ public class ScrollCamCtrl : MonoBehaviour {
 		//scroll based on mouse movement (wrong)
 		float width = Screen.width;
 		float height = Screen.height;
-		float left = padding;
-		float right = width - padding;
-		float bottom = padding;
-		float top = height - padding;
+		float left = m_padding;
+		float right = width - m_padding;
+		float bottom = m_padding;
+		float top = height - m_padding;
 		bool west = m_mousePos.x < left;
 		bool east = m_mousePos.x > right;
 		bool north = m_mousePos.y > top;
