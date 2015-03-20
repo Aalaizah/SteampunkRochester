21762643ff504a0e1657662c5e6fda7338bd9a80
unknown <tls8963@GDD-50.main.ad.rit.edu>
2015-03-20 15:10:10 -0400

    Map Scene, Markers, Level Editing Scene, Some Such, etc.

---
+++ Unity/Assets/Scripts/Behaviours/MarkerBhv.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MarkerBhv : MonoBehaviour {
+	GameObject m_tack;
+	GameObject m_thumbnail;
+	GameObject m_map;
+	Vector2 m_mousePosition;
+	public GameObject m_levelToLoad;
+
+	// Use this for initialization
+	void Start () {
+		m_tack = transform.Find("Tack").gameObject;
+		m_thumbnail = transform.Find("Thumbnail").gameObject;
+		m_map = GameObject.Find("Map");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_mousePosition = Input.mousePosition;
+		Camera cam = Camera.main;
+		//convert the mouse to world coordinates
+		Vector2 newMouse = cam.ScreenToWorldPoint(m_mousePosition);
+		//check if the mouse contains
+		if(m_tack.collider2D.bounds.Contains(newMouse))
+		{
+			m_thumbnail.SetActive(true);
+			if(Input.GetMouseButtonDown(0))
+			{
+				//do something when tack is clicked
+				ClickAction();
+			}
+		}else{
+			m_thumbnail.SetActive(false);
+		}
+	}
+
+	void ClickAction()
+	{
+		if(m_levelToLoad == null)
+			return;
+		//disable the map
+		m_map.SetActive(false);
+		//load level
+		Instantiate(m_levelToLoad);
+		//todo, add player and disable map
+	}
+}
