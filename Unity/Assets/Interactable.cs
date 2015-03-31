93e9380e65ecf29ffcf4f810a73bc028bb13c94d
JosephCoppola <jjc9718@rit.edu>
2015-03-30 23:40:56 -0400

    Choices Logic Set Up in Interactable

--- Unity/Assets/Interactable.cs
+++ Unity/Assets/Interactable.cs
@@ -2,151 +2,204 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Interactable : MonoBehaviour {
-	
-	private SpriteRenderer spriteRenderer;
-	public Sprite idleSprite;
-	public Sprite hoverSprite;
-	public Sprite activeSprite;
-	public string id;
-	public string path;
-	private bool clicked;
-	public bool selected;
-	private bool readTwine;
-	TwineImporter Twine;
-	string message = "";
-	string currentNode = "0";
-	public bool taken = false;
-	public bool isItem;
-	//string speaker = "";
-	
-	void Start()
-	{
-		gameObject.AddComponent<BoxCollider2D>();
-		spriteRenderer = GetComponent<SpriteRenderer>();
-		Twine =  GameObject.Find("TwineImporter").GetComponent<TwineImporter> ();
-		Twine.ReadTwineFile(path);
-		createMessage();
-	}
+public class Interactable : MonoBehaviour
+{
 
-	public void Progress(Inventory i)
-	{
-		/*Twine.TwineData.NextNode(currentNode);
-		createMessage ();
-		if (Twine.TwineData.Current.Link.Count != 0)
-		{
-			currentNode = Twine.TwineData.Current.Link[0];
-		}*/
-		Twine.TwineData.NextNode(currentNode);
-		//Debug.Log (Twine.TwineData.Current.itemsReq);
-		if (!Twine.TwineData.Current.itemsReq.Equals("")) 
-		{
-			Debug.Log("Item Required");
-			if(i.inventory.Count!=0){
-				for (int x=0; x < i.inventory.Count; x++) 
-				{
-					//Debug.Log(Twine.TwineData.Current.itemsReq.Contains(i.inventory[x]));
-					if (Twine.TwineData.Current.itemsReq.Contains(i.inventory[x])) 
-					{
-						createMessage ();
-						if (Twine.TwineData.Current.Link.Count != 0) 
-						{
-							//Debug.Log(Twine.TwineData.Current.Link [0]);
-							currentNode = Twine.TwineData.Current.Link [0];
-						}
-						break;
-					}	
-				}
-				
-			}
-		} 
-		else 
-		{
-			//Debug.Log("No item required!");
-			createMessage ();
-			if (Twine.TwineData.Current.Link.Count != 0) 
-			{
-				//Debug.Log(Twine.TwineData.Current.Link [0]);
-				currentNode = Twine.TwineData.Current.Link [0];
-			}
-		}
+    private SpriteRenderer spriteRenderer;
+    public Sprite idleSprite;
+    public Sprite hoverSprite;
+    public Sprite activeSprite;
+    public string id;
+    public string path;
+    private bool clicked;
+    public bool selected;
+    private bool readTwine;
+    TwineImporter Twine;
+    string message = "";
+    string currentNode = "0";
+    public bool taken = false;
+    public bool isItem;
+    public List<string> choicesList;
+    public List<string> choicesLinksList;
+    public bool choice = false;
 
-	}
+    //string speaker = "";
 
-	void Update()
-	{
-		
-	}
+    void Start()
+    {
+        gameObject.AddComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Twine = GameObject.Find("TwineImporter").GetComponent<TwineImporter>();
+        Twine.ReadTwineFile(path);
+        createMessage();
+    }
 
-	void OnGUI() {
-		if(selected)
-		{
-			GUI.skin.box.wordWrap = true;
-			GUI.Box (new Rect (Screen.width - (Screen.width - 5), 3 * (Screen.height / 4) - 5, Screen.width - 10, Screen.height / 4),message);
-		}
-	}
-	
-	void OnMouseOver()
-	{
-		if(!clicked)
-		{
-			spriteRenderer.sprite = hoverSprite;
-		}
-	}
-	
-	void OnMouseExit()
-	{
-		clicked = false;
-		spriteRenderer.sprite = idleSprite;
-	}
-	
-	void OnMouseDown()
-	{
-		if(!clicked)
-		{
+    public void Progress(Inventory i)
+    {
+        /*Twine.TwineData.NextNode(currentNode);
+        createMessage ();
+        if (Twine.TwineData.Current.Link.Count != 0)
+        {
+            currentNode = Twine.TwineData.Current.Link[0];
+        }*/
+        Twine.TwineData.NextNode(currentNode);
+        //Debug.Log (Twine.TwineData.Current.itemsReq);
+        if (!Twine.TwineData.Current.itemsReq.Equals(""))
+        {
+            Debug.Log("Item Required");
+            if (i.inventory.Count != 0)
+            {
+                for (int x = 0; x < i.inventory.Count; x++)
+                {
+                    //Debug.Log(Twine.TwineData.Current.itemsReq.Contains(i.inventory[x]));
+                    if (Twine.TwineData.Current.itemsReq.Contains(i.inventory[x]))
+                    {
+                        createMessage();
+                        if (Twine.TwineData.Current.Link.Count != 0)
+                        {
+                            //Debug.Log(Twine.TwineData.Current.Link [0]);
+                            currentNode = Twine.TwineData.Current.Link[0];
+                        }
+                        break;
+                    }
+                }
 
-			clicked = true;
-			spriteRenderer.sprite = activeSprite;
-			selected = !selected;
-			Debug.Log(selected);
-			if(!selected){
-				currentNode = "0";
-				taken = true;
+            }
+        }
+        else
+        {
+            //Debug.Log("No item required!");
+            createMessage();
+            if (Twine.TwineData.Current.Link.Count != 0)
+            {
+                //Debug.Log(Twine.TwineData.Current.Link [0]);
+                currentNode = Twine.TwineData.Current.Link[0];
+            }
+        }
 
-			}
-		}
-	}
-	
-	void OnMouseUp()
-	{
-		clicked = false;
-	}
-	
-	public void SetName(string pName,string pPath)
-	{
-		id = pName;
-		path = pPath;
-		spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
-		idleSprite = Resources.Load<Sprite>(id + "Idle");
-		hoverSprite = Resources.Load<Sprite>(id + "Hover");
-		activeSprite = Resources.Load<Sprite>(id + "Active");
-		
-		spriteRenderer.sprite = idleSprite;
-	}
+    void Update()
+    {
 
-	void createMessage()
-	{
-		message = "";
-		
-		//speaker = Twine.TwineData.Current.SpeakerData;
+    }
 
-		foreach (char letter in Twine.TwineData.Current.ContentData.ToString()) 
-		{
-			message += letter;
-			//yield return new WaitForSeconds (letterPause);
-		}
-		
-		//contentList.Clear();
-	}
+    void OnGUI()
+    {
+        if (selected)
+        {
+            GUI.skin.box.wordWrap = true;
+            if (choice == false)
+            {
+                GUI.Box(new Rect(Screen.width - (Screen.width - 5), 3 * (Screen.height / 4) - 5, Screen.width - 10, Screen.height / 4), message);
+            }
+            else if (choice == true)
+            {
+                for (int i = 0; i <= choicesLinksList.Count; i++)
+                {
+                    if (GUI.Button(new Rect(Screen.width - (Screen.width - 5), 3 * (Screen.height / choicesLinksList.Count) + i * (Screen.height / choicesLinksList.Count / choicesLinksList.Count),
+                                           Screen.width - 10, Screen.height / choicesLinksList.Count / choicesLinksList.Count), choicesList[i]))
+                    {
+                        Twine.TwineData.NextNode(choicesLinksList[i]);
+                        Twine.TwineData.NextNode(Twine.TwineData.Current.LinkData);
+                        StartCoroutine(createMessage());
+                        Twine.TwineData.NextNode(Twine.TwineData.Current.LinkData);
+                        choice = false;
+                    }
+                }
+            }
+        }
+    }
+
+    void OnMouseOver()
+    {
+        if (!clicked)
+        {
+            spriteRenderer.sprite = hoverSprite;
+        }
+    }
+
+    void OnMouseExit()
+    {
+        clicked = false;
+        spriteRenderer.sprite = idleSprite;
+    }
+
+    void OnMouseDown()
+    {
+        if (!clicked)
+        {
+
+            clicked = true;
+            spriteRenderer.sprite = activeSprite;
+            selected = !selected;
+            //Debug.Log(selected);
+            if (!selected)
+            {
+                currentNode = "0";
+                taken = true;
+
+            }
+        }
+    }
+
+    void OnMouseUp()
+    {
+        clicked = false;
+    }
+
+    public void SetName(string pName, string pPath)
+    {
+        id = pName;
+        path = pPath;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        idleSprite = Resources.Load<Sprite>(id + "Idle");
+        hoverSprite = Resources.Load<Sprite>(id + "Hover");
+        activeSprite = Resources.Load<Sprite>(id + "Active");
+
+        spriteRenderer.sprite = idleSprite;
+    }
+
+
+
+    IEnumerator createMessage()
+    {
+        message = "";
+
+        //speaker = Twine.TwineData.Current.SpeakerData;
+
+        foreach (char letter in Twine.TwineData.Current.ContentData.ToString())
+        {
+            message += letter;
+            //yield return new WaitForSeconds (letterPause);
+        }
+
+        //contentList.Clear();
+
+        TypeText();
+    }
+
+    void TypeText()
+    {
+        TwineNode tempNode;
+        if (Twine.TwineData.Current.LinkTitle.Count == 1)
+        {
+            choice = false;
+            StartCoroutine(createMessage());
+            Twine.TwineData.NextNode(Twine.TwineData.Current.LinkData);
+        }
+        else
+        {
+            choice = true;
+            tempNode = Twine.TwineData.Current;
+            foreach (string currentChoice in Twine.TwineData.Current.Link)
+            {
+                Twine.TwineData.NextNode(currentChoice);
+                choicesLinksList.Add(currentChoice);
+                choicesList.Add(Twine.TwineData.Current.ContentData);
+            }
+            Twine.TwineData.Current = tempNode;
+        }
+    }
 }
