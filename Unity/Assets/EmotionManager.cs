9819ce2dfcb187293a581ba45990f4debd939ea0
unknown <tls8963@OH1380-25.main.ad.rit.edu>
2015-05-05 10:41:27 -0400

    Emoticon particles.

--- Unity/Assets/EmotionManager.cs
+++ Unity/Assets/EmotionManager.cs
@@ -3,14 +3,36 @@ using System.Collections;
 using System.Collections.Generic;
 
 public static class EmotionManager{
+	//one intialize
+	static bool initialized;
 	//dictionary to contain the characters and their respecitve happiness towards the player character
 	private static Dictionary<string,int> emotionValue = new Dictionary<string,int>();
 	private static bool alreadyChangedUp = false;
 	private static bool alreadyChangedDwn = false;
+	//our emotion emitters
+	private static GameObject happyFx;
+	private static GameObject sadFx;
+
+	//Initializes only if its not already done
+	public static bool TryInitialize()
+	{
+		if(initialized)
+			return false;
+		var happy = Resources.Load("Particles/HappyFx");
+		var sad = Resources.Load("Particles/SadFx");
+		Debug.Log(happy);
+		Debug.Log(sad);
+		happyFx = Object.Instantiate(happy) as GameObject;
+		sadFx = Object.Instantiate(sad) as GameObject;
+		initialized = true;
+		return true;
+	}
 
 	//takes in the person to update and whether to make them happier or angrier
 	public static void updateEmotions(string person, bool upOrDown)
 	{
+		TryInitialize();
+
 		//adds the person to the dictionary if they haven't been added yet
 		if(!emotionValue.ContainsKey(person))
 		{
@@ -20,10 +42,14 @@ public static class EmotionManager{
 		//adds or subtracts from the value as per the boolean parameter
 		if(upOrDown && !alreadyChangedUp){
 			emotionValue[person]++;
+			//trigger happy emoticon
+			happyFx.particleSystem.Play();
 			alreadyChangedUp = true;
 		}
 		else if(!upOrDown && !alreadyChangedDwn){
 			emotionValue[person]--;
+			//trigger sad emoticon
+			sadFx.particleSystem.Play();
 			alreadyChangedDwn = true;
 		}
 
