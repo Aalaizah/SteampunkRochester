b6752e66d636aa5fe44b318c6eedfe9c831fe24e
unknown <jjc9718@GDD-26.main.ad.rit.edu>
2015-05-10 15:41:39 -0400

    Arrows added.

--- Unity/Assets/Scripts/Controllers/ScrollCamCtrl.cs
+++ Unity/Assets/Scripts/Controllers/ScrollCamCtrl.cs
@@ -5,26 +5,19 @@ public class ScrollCamCtrl : MonoBehaviour {
 	ScrollCamBhv m_scrollBhv;
 	Vector2 m_mousePos;
 	public float m_padding = 25;
-	
-	void OnGUI(){
-		//create rectangles to indicate scroll regions
-		//Adapt these to the screens resolution
-		float innerBot = Screen.height - m_padding;
-		float innerRight = Screen.width - m_padding;
-		Rect topBar = new Rect (0, 0, Screen.width, m_padding);
-		Rect bottomBar = new Rect (0, innerBot, Screen.width, m_padding);
-		Rect leftBar = new Rect (0, m_padding, m_padding, innerBot - m_padding);
-		Rect rightBar = new Rect (innerRight, m_padding, m_padding, innerBot - m_padding);
-		//draw these rectangles as boxes
-		GUI.Box (topBar, string.Empty);
-		GUI.Box (bottomBar, string.Empty);
-		GUI.Box (leftBar, string.Empty);
-		GUI.Box (rightBar, string.Empty);
-	}
+	public float arrowNormalOpacity = 0.2f;
+	public float arrowHoverOpacity = 0.8f;
+	SpriteRenderer arrowRight;
+	SpriteRenderer arrowLeft;
 
 	// Use this for initialization
 	void Start () {
 		m_scrollBhv = gameObject.GetComponent<ScrollCamBhv>();
+		//find the arrows so we don't have to find them in the levelEditor scene
+		var arrowR = transform.FindChild ("arrowRight").gameObject;
+		var arrowL = transform.FindChild ("arrowLeft").gameObject;
+		arrowRight = arrowR.GetComponent<SpriteRenderer> ();
+		arrowLeft = arrowL.GetComponent<SpriteRenderer> ();
 	}
 	
 	// Update is called once per frame
@@ -53,6 +46,18 @@ public class ScrollCamCtrl : MonoBehaviour {
 		bool east = m_mousePos.x > right;
 		bool north = m_mousePos.y > top;
 		bool south = m_mousePos.y < bottom;
+		Color normal = new Color (1.0f, 1.0f, 1.0f, arrowNormalOpacity);
+		Color hover = new Color (1.0f, 1.0f, 1.0f, arrowHoverOpacity);
+		if (east) {
+			arrowRight.color = hover;
+		} else {
+			arrowRight.color = normal;
+				}
+		if (west) {
+			arrowLeft.color = hover;
+		} else {
+			arrowLeft.color = normal;
+				}
 		m_scrollBhv.Scroll(east, west, north, south);
 	}
 }
