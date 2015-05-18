2a6a19e31ed1f1c539768cae6d1099c167b94fcc
JosephCoppola <jjc9718@g.rit.edu>
2015-05-18 16:45:34 -0400

    Menu is working now I swear Tyler

--- Unity/Assets/Scripts/UIManager.cs
+++ Unity/Assets/Scripts/UIManager.cs
@@ -65,34 +65,4 @@ public class UIManager : MonoBehaviour {
 	public void Quit(){
 		Application.Quit ();
 	}
-
-	public void Credits(){
-
-		if(menuButtons == null)
-		{
-			menuButtons = GameObject.FindGameObjectsWithTag("MenuUI");
-		}
-		
-		for(int i = 0; i < menuButtons.Length; i++)
-		{
-			menuButtons[i].SetActive(false);
-		}
-
-		creditsPrefab.SetActive (true);
-	}
-
-	public void CreditReturn()
-	{
-		creditsPrefab.SetActive (false);
-
-		for(int i = 0; i < menuButtons.Length; i++)
-		{
-			menuButtons[i].SetActive(true);
-		}
-	}
-
-	public void MainMenuStart(){
-		//Load Main scene here
-	
-	}
 }
